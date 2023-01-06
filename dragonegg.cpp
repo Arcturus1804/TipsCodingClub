cbb33cf18605d25bb0c1f25da180017a0a94e9ac
Arcturus1804 <117738587+Arcturus1804@users.noreply.github.com>
2023-01-06 21:04:08 +0530

    Update dragonegg.cpp

--- dragonegg.cpp
+++ dragonegg.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){int gold{},goldc{},silver{},silverc{};
-cout<<"Enter grams of gold"<<endl;;
+cout<<"Enter grams of gold"<<endl;
 cin>>gold;
-cout<<"Enter price of gold"<<endl;;
+cout<<"Enter price of gold"<<endl;
 cin>>goldc;
-cout<<"Enter grams of silver"<<endl;;
+cout<<"Enter grams of silver"<<endl;
 cin>>silver;
-cout<<"Enter price of silver"<<endl;;
+cout<<"Enter price of silver"<<endl;
 cin>>silverc;
 if(gold*goldc>=silver*silverc)
 cout<<"Gold";
